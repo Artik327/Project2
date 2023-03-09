7cdc8becc2b406630f34bb427abe2d83aa5f7065
Artik327 <artur.g1810@gmail.com>
2023-03-09 19:41:47 +0100

    finished footer

--- js.js
+++ js.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $("#cross").click(function(){
         $(".modal-window").hide();
     });
+
+    $(".save").click(function(){
+        $(".modal-window").hide();
+    });
 });
